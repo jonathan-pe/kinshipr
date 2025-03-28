c90ffaed248bf50af2f27c7e67cd7aff66b410cc
Jonathan Pe <jonathanqpe@gmail.com>
2025-03-28 15:47:09 -0700

    add login placeholder to the landing page for easier navigation

--- client/src/modules/_nonAuthed/index.tsx
+++ client/src/modules/_nonAuthed/index.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_nonAuthed/')({
   component: Home,
 })
 
 function Home() {
-  return <div className='flex flex-1 flex-col items-center justify-center mx-auto'>Landing Page</div>
+  return (
+    <div className='flex flex-1 flex-col items-center justify-center mx-auto'>
+      Landing Page
+      <Link to='/login'>Login</Link>
+    </div>
+  )
 }
