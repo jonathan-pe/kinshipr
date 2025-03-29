9dde1ed43b3628566cdefdc075e67c881eacd2b2
Jonathan Pe <jonathanqpe@gmail.com>
2025-03-29 15:38:22 -0700

    remove navbar for unauthed routes

--- client/src/modules/_nonAuthed/route.tsx
+++ client/src/modules/_nonAuthed/route.tsx
@@ -1,4 +1,3 @@
-import ThemeToggle from '@/components/theme-toggle'
 import { GoogleOneTap } from '@clerk/clerk-react'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
@@ -16,10 +15,6 @@ export const Route = createFileRoute('/_nonAuthed')({
 function RouteComponent() {
   return (
     <div className='container min-h-screen flex flex-col items-center justify-center mx-auto'>
-      <header className='flex justify-between items-center w-full p-4'>
-        <h2>kinshipr</h2>
-        <ThemeToggle />
-      </header>
       <Outlet />
       <GoogleOneTap />
     </div>
