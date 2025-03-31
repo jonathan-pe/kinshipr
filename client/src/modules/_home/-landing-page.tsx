f2437b90cef0aaf05c35722979b1cb381769c9ef
Jonathan Pe <jonathanqpe@gmail.com>
2025-03-31 13:51:00 -0700

    fix landing page layout

--- client/src/modules/_home/-landing-page.tsx
+++ client/src/modules/_home/-landing-page.tsx
@@ -5,7 +5,7 @@ import { GoogleOneTap } from '@clerk/clerk-react'
 export default function LandingPage() {
   return (
     <>
-      <div className='flex flex-1 justify-center items-center w-full space-x-40'>
+      <div className='flex flex-1 justify-center items-center w-full gap-40 flex-wrap p-4 mx-auto'>
         <div className='flex flex-col items-center justify-center space-y-4'>
           <KinshiprLogo className='w-96 h-auto' />
           <h1 className='text-3xl font-bold'>Kinshipr</h1>
