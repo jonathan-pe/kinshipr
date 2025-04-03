68d49679abbe3b98e61def9ee1d4ecf5f7cebc2c
Jonathan Pe <jonathanqpe@gmail.com>
2025-04-03 16:06:15 -0700

    move edit profile button closer to tabs in profile page

--- client/src/modules/_authed/profile/index.tsx
+++ client/src/modules/_authed/profile/index.tsx
@@ -29,41 +29,42 @@ function RouteComponent() {
 
   return (
     <div className='flex flex-col flex-1 m-4 gap-4'>
-      <div className='flex justify-between'>
-        <Tabs value={selectedTab} onValueChange={setSelectedTab} className='flex flex-1'>
+      <Tabs value={selectedTab} onValueChange={setSelectedTab} className='flex flex-1'>
+        <div className='flex items-center gap-4'>
           <TabsList>
             <TabsTrigger value='profile'>Profile</TabsTrigger>
             <TabsTrigger value='account'>Account</TabsTrigger>
           </TabsList>
-          <TabsContent value='profile' className='flex flex-col mt-4 gap-4'>
-            <div className='flex items-center gap-8'>
-              <Avatar className='h-32 w-32 rounded-full'>
-                <AvatarImage src={profilePictureUrl} />
-                <AvatarFallback className='rounded-full'>
-                  <User size={80} />
-                  <span className='sr-only'>{`${displayName || username}'s profile picture`}</span>
-                </AvatarFallback>
-              </Avatar>
-              <div className='flex flex-col gap-2'>
-                {displayName && <h1>{displayName}</h1>}
-                <h2>@{username}</h2>
-                <p className='text-muted-foreground'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
-                </p>
-              </div>
-            </div>
-          </TabsContent>
-          <TabsContent value='account' className='flex flex-col mt-4 justify-center items-center'>
-            <UserProfile appearance={{ baseTheme: theme === 'dark' ? dark : undefined }} />
-          </TabsContent>
-        </Tabs>
 
-        {selectedTab === 'profile' && <Button>Edit Profile</Button>}
-      </div>
+          {selectedTab === 'profile' && <Button>Edit Profile</Button>}
+        </div>
+
+        <TabsContent value='profile' className='flex flex-col mt-4 gap-4'>
+          <div className='flex items-center gap-8'>
+            <Avatar className='h-32 w-32 rounded-full'>
+              <AvatarImage src={profilePictureUrl} />
+              <AvatarFallback className='rounded-full'>
+                <User size={80} />
+                <span className='sr-only'>{`${displayName || username}'s profile picture`}</span>
+              </AvatarFallback>
+            </Avatar>
+            <div className='flex flex-col gap-2'>
+              {displayName && <h1>{displayName}</h1>}
+              <h2>@{username}</h2>
+              <p className='text-muted-foreground'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value='account' className='flex flex-col mt-4 justify-center items-center'>
+          <UserProfile appearance={{ baseTheme: theme === 'dark' ? dark : undefined }} />
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
