196364a783a11a0ad91bd2df38e78802410a4461
Jonathan Pe <jonathanqpe@gmail.com>
2025-04-02 22:25:38 -0700

    set up tabs within the logged in user's profile to enable account management as well

--- client/src/modules/_authed/profile/index.tsx
+++ client/src/modules/_authed/profile/index.tsx
@@ -1,7 +1,8 @@
 import { useUserProfile } from '@/api/profiles'
 import Pending from '@/components/pending'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { useAuth } from '@clerk/clerk-react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { useAuth, UserProfile } from '@clerk/clerk-react'
 import { createFileRoute } from '@tanstack/react-router'
 import { User } from 'lucide-react'
 
@@ -21,30 +22,41 @@ function RouteComponent() {
 
   return (
     <div className='flex flex-col flex-1 m-4 gap-4'>
-      <header className='flex items-center gap-4'>
-        <Avatar className='h-40 w-40 rounded-full'>
-          <AvatarImage src={profilePictureUrl} />
-          <AvatarFallback className='rounded-full'>
-            <User size={100} />
-            <span className='sr-only'>{`${displayName || username}'s profile picture`}</span>
-          </AvatarFallback>
-        </Avatar>
-        <div className='flex flex-col'>
-          {displayName && <h1>{displayName}</h1>}
-          <h1>@{username}</h1>
-        </div>
-      </header>
+      <Tabs defaultValue='profile'>
+        <TabsList>
+          <TabsTrigger value='profile'>Profile</TabsTrigger>
+          <TabsTrigger value='account'>Account</TabsTrigger>
+        </TabsList>
+        <TabsContent value='profile' className='flex flex-col mt-4 gap-4'>
+          <div className='flex items-center gap-4'>
+            <Avatar className='h-40 w-40 rounded-full'>
+              <AvatarImage src={profilePictureUrl} />
+              <AvatarFallback className='rounded-full'>
+                <User size={100} />
+                <span className='sr-only'>{`${displayName || username}'s profile picture`}</span>
+              </AvatarFallback>
+            </Avatar>
+            <div className='flex flex-col'>
+              {displayName && <h1>{displayName}</h1>}
+              <h1>@{username}</h1>
+            </div>
+          </div>
 
-      <div className='flex flex-col gap-2 mx-2'>
-        <h2>Bio</h2>
-        <p className='text-muted-foreground'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
-        </p>
-      </div>
+          <div className='flex flex-col gap-2 mx-2'>
+            <h2>Bio</h2>
+            <p className='text-muted-foreground'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </p>
+          </div>
+        </TabsContent>
+        <TabsContent value='account' className='flex flex-col mt-4 justify-center items-center'>
+          <UserProfile />
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
